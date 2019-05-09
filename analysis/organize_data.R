ef57159d7acf6a076058cc26d16432a6fc5735ea
Mary Follo <mfollo@uoregon.edu>
2019-05-09 12:39:50 -0700

    Updates: library

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -6,10 +6,11 @@
 # This script will read in raw data from the input directory, clean it up to produce 
 # the analytical dataset, and then write the analytical data to the output directory. 
 
-
+library(haven) 
+library(readr)
 dat <- read_dta(file= "input/anes_timeseries_2016_dta.zip")
 dat <- read_dta(file= "input/anes_timeseries_2012_dta.zip")
-dat <- read.csv(file = "input/anes_timeseries_2016_methodology_csv.zip")
+
 dat <- read.csv(file= "input/anes_timeseries_2016_voteval.csv.zip")
 dat <- read.csv(file= "input/anes_timeseries_2012_voteval.csv.zip")
 
